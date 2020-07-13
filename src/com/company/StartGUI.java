786929a40daa86887764a4d0a424586f03d089cb
Ilya Tsimkovskiy <itsimkovskiy@gmail.com>
2020-07-13 23:07:33 +0300

    v1.0.1 t9 almost ready, 2 bags found

--- src/com/company/StartGUI.java
+++ src/com/company/StartGUI.java
@@ -4,14 +4,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+/*
+Необходимо доработать звездочку
+Добавить кнопку которая удаляет только последний символ.
+Нашел 2 баги:
+1) При нажатии кнопки n раз, следующее нажатие через большой промежуток времени добавит символ не n[0], а n+1
+2) При нажатии нескольких кнопок очень быстро они перезатирают предыдущее значение
+*/
 public class StartGUI {
     private final String[] buttonsName = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0","#"};
     private final JButton[] buttons = new JButton[buttonsName.length];
     private JLabel messageLabel;
     private int count = 1;
     private String previousButtonText="";
-    private Timer timer;
+    private long endTime;
+    private String currentMsg;
 
     public static void main(String[] args) {
         StartGUI gui = new StartGUI();
@@ -20,12 +27,12 @@ public class StartGUI {
     private void createAndShowGUI(){
         JFrame frame = new JFrame("NOKLA");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(316,300));
+        frame.setPreferredSize(new Dimension(279+8,300));
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 
 
         JPanel menuPanel = new JPanel();
-        menuPanel.setPreferredSize(new Dimension(300,120));
+        menuPanel.setPreferredSize(new Dimension(287,120));
         menuPanel.setLayout(new GridBagLayout());
         menuPanel.setBackground(Color.pink);
         menuPanel.setVisible(true);
@@ -48,21 +55,29 @@ public class StartGUI {
         int columns_of_buttons = 3;
         int rows_of_buttons = buttonsName.length/columns_of_buttons; //4
         int buttonsNameK = 0;
-        JButton[][] buttonsTest = new JButton[rows_of_buttons][columns_of_buttons];
         for (int i = 0; i < rows_of_buttons; i++){
             for(int j = 0; j < columns_of_buttons; j++){
-                JButton buttonTest = new JButton(buttonsName[buttonsNameK]);
-                buttonTest.setPreferredSize(new Dimension(90,40));
-                buttonTest.addActionListener(new ButtonListener(buttonsName[buttonsNameK]));
-                menuPanel.add(buttonTest,
+                JButton button = new JButton(buttonsName[buttonsNameK]);
+                button.setPreferredSize(new Dimension(90,40));
+                button.addActionListener(new ButtonListener(buttonsName[buttonsNameK]));
+                menuPanel.add(button,
                         new GridBagConstraints(
                                 j, i + dY, 1, 1, 1,1,
                                 GridBagConstraints.CENTER, GridBagConstraints.CENTER,
                                 new Insets(0,0,0,0),0,0));
-                buttonTest.setVisible(true);
+                button.setVisible(true);
                 buttonsNameK++;
             }
         }
+        JButton clearButton = new JButton("Clear");
+        clearButton.setPreferredSize(new Dimension(90,40));
+        clearButton.addActionListener(new ButtonListener(clearButton.getText().toLowerCase()));
+        menuPanel.add(clearButton,
+                new GridBagConstraints(
+                        0, 8, 3, 1, 3,1,
+                        GridBagConstraints.CENTER, GridBagConstraints.CENTER,
+                        new Insets(0,0,0,0),0,0));
+        clearButton.setVisible(true);
 
         frame.add(menuPanel);
         frame.setLocationRelativeTo(null);
@@ -73,14 +88,11 @@ public class StartGUI {
 
     class ButtonListener implements ActionListener {
         private String buttonText;
-        private long endTime;
-        private long startTime;
+        long startTime = System.currentTimeMillis();
 
         public ButtonListener(String buttonText){
-            endTime = System.currentTimeMillis();
             this.buttonText = buttonText;
-//            timer = new Timer(300, this);
-//            timer.start();
+            currentMsg = messageLabel.getText();
         }
 
         private void counterCheck(int arrayLength){
@@ -89,68 +101,84 @@ public class StartGUI {
             }
         }
 
+        private String timeCheck(long duration, String symbol){
+            if (duration >= 500){
+                currentMsg = messageLabel.getText();
+                count = 0;
+            } else {
+                if (duration >= 0){
+                    startTime = System.currentTimeMillis();
+                }
+            }
+            count++;
+            return currentMsg + symbol;
+        }
+
         public void actionPerformed(ActionEvent e) {
+            startTime = System.currentTimeMillis();
+            long duration = 0;
+            String symbol = "";
+            if (endTime != 0)
+                {duration = startTime - endTime;}
             switch (buttonText) {
                 case "1": {
                     String[] digit_1_values = {".",",","-","?","!","@","(",")","/","1"};
                     counterCheck(digit_1_values.length);
-                    //if (duration > 0 && duration < 500){
-                        messageLabel.setText(digit_1_values[count-1]);
-                  //  }
+                    symbol = digit_1_values[count-1];
                     break;
                 }
                 case "2": {
                     String[] digit_2_values = {"а","б","в","г","2"};
                     counterCheck(digit_2_values.length);
-                    messageLabel.setText(messageLabel.getText()+digit_2_values[count-1]);
+                    symbol = digit_2_values[count-1];
                     break;
                 }
                 case "3": {
                     String[] digit_3_values = {"д","е","ё","ж","з","3"};
                     counterCheck(digit_3_values.length);
-                    messageLabel.setText(messageLabel.getText()+digit_3_values[count-1]);
+                    symbol = digit_3_values[count-1];
                     break;
                 }
                 case "4": {
                     String[] digit_4_values = {"и","й","к","л","4"};
                     counterCheck(digit_4_values.length);
-                    messageLabel.setText(messageLabel.getText()+digit_4_values[count-1]);
+                    symbol = digit_4_values[count-1];
                     break;
                 }
                 case "5": {
                     String[] digit_5_values = {"м","н","о","п","5"};
                     counterCheck(digit_5_values.length);
-                    messageLabel.setText(messageLabel.getText()+digit_5_values[count-1]);
+                    symbol = digit_5_values[count-1];
                     break;
                 }
                 case "6": {
                     String[] digit_6_values = {"р","с","т","у","6"};
                     counterCheck(digit_6_values.length);
-                    messageLabel.setText(messageLabel.getText()+digit_6_values[count-1]);
+                    symbol = digit_6_values[count-1];
                     break;
                 }
                 case "7": {
                     String[] digit_7_values = {"ф","х","ц","ч","7"};
                     counterCheck(digit_7_values.length);
-                    messageLabel.setText(messageLabel.getText()+digit_7_values[count-1]);
+                    symbol = digit_7_values[count-1];
                     break;
                 }
                 case "8": {
                     String[] digit_8_values = {"ш","щ","Ъ","ы","8"};
                     counterCheck(digit_8_values.length);
-                    messageLabel.setText(messageLabel.getText()+digit_8_values[count-1]);
+                    symbol = digit_8_values[count-1];
                     break;
                 }
                 case "9": {
                     String[] digit_9_values = {"ь","э","ю","я","9"};
                     counterCheck(digit_9_values.length);
-                    messageLabel.setText(messageLabel.getText()+digit_9_values[count-1]);
+                    symbol = digit_9_values[count-1];
                     break;
                 }
                 case "0": {
                     String[] digit_0_values = {"+","=","<",">","$","%","0"};
                     counterCheck(digit_0_values.length);
-                    messageLabel.setText(messageLabel.getText()+digit_0_values[count-1]);
+                    symbol = digit_0_values[count-1];
                     break;
                 }
                 case "*": {
@@ -167,25 +195,15 @@ public class StartGUI {
                     messageLabel.setText(messageLabel.getText()+" ");
                     break;
                 }
+
+                case "clear": {
+                    messageLabel.setText("");
+                }
             }
             previousButtonText = this.buttonText;
-            count++;
-
+            String t = timeCheck(duration, symbol);
+            messageLabel.setText(t);
+            endTime = System.currentTimeMillis();
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
