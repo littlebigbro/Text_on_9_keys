4021d99d42819d37b6389675e79de5584301d854
Ilya Tsimkovskiy <itsimkovskiy@gmail.com>
2020-07-18 18:11:46 +0300

    pre_build

--- src/com/company/StartGUI.java
+++ src/com/company/StartGUI.java
@@ -1,209 +1,236 @@
 package com.company;
 
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-/*
-Необходимо доработать звездочку
-Добавить кнопку которая удаляет только последний символ.
-Нашел 2 баги:
-1) При нажатии кнопки n раз, следующее нажатие через большой промежуток времени добавит символ не n[0], а n+1
-2) При нажатии нескольких кнопок очень быстро они перезатирают предыдущее значение
-*/
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public class StartGUI {
-    private final String[] buttonsName = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0","#"};
-    private final JButton[] buttons = new JButton[buttonsName.length];
-    private JLabel messageLabel;
-    private int count = 1;
-    private String previousButtonText="";
-    private long endTime;
-    private String currentMsg;
+    private final String[] buttonsName = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0","#","Clear","Backspace", "Enter"};
+    private final String[] valuesOfButton0 = {"+","=","<",">","$","%","0"};
+    private final String[] valuesOfButton1 = {".",",","-","?","!","@","(",")","/","1"};
+    private final String[] valuesOfButton2 = {"а","б","в","г","2"};
+    private final String[] valuesOfButton3 = {"д","е","ё","ж","з","3"};
+    private final String[] valuesOfButton4 = {"и","й","к","л","4"};
+    private final String[] valuesOfButton5 = {"м","н","о","п","5"};
+    private final String[] valuesOfButton6 = {"р","с","т","у","6"};
+    private final String[] valuesOfButton7 = {"ф","х","ц","ч","7"};
+    private final String[] valuesOfButton8 = {"ш","щ","ъ","ы","8"};
+    private final String[] valuesOfButton9 = {"ь","э","ю","я","9"};
+    private final String[] valuesOfButtonStar = {"Abc", "abc", "ABC", "123"};
+    private final Map<String,String[]> buttonsParams = new HashMap<>();
+
+    private JTextComponent message;
+    private JLabel typeMode;
+    private int count = 0;
+    private int countMode = 0;
+    private String previousButtonName="";
+    private long lastTime;
+
+    public void loadMap(){
+        buttonsParams.put("0",valuesOfButton0);
+        buttonsParams.put("1",valuesOfButton1);
+        buttonsParams.put("2",valuesOfButton2);
+        buttonsParams.put("3",valuesOfButton3);
+        buttonsParams.put("4",valuesOfButton4);
+        buttonsParams.put("5",valuesOfButton5);
+        buttonsParams.put("6",valuesOfButton6);
+        buttonsParams.put("7",valuesOfButton7);
+        buttonsParams.put("8",valuesOfButton8);
+        buttonsParams.put("9",valuesOfButton9);
+        buttonsParams.put("*",null);
+        buttonsParams.put("#",null);
+        buttonsParams.put("Clear", null);
+        buttonsParams.put("Backspace", null);
+        buttonsParams.put("Enter", null);
+    }
 
     public static void main(String[] args) {
         StartGUI gui = new StartGUI();
+        gui.loadMap();
         gui.createAndShowGUI();
     }
     private void createAndShowGUI(){
+
         JFrame frame = new JFrame("NOKLA");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(279+8,300));
+        frame.setPreferredSize(new Dimension(279+8,356));
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 
-
         JPanel menuPanel = new JPanel();
-        menuPanel.setPreferredSize(new Dimension(287,120));
+        menuPanel.setPreferredSize(new Dimension(287,219));
         menuPanel.setLayout(new GridBagLayout());
         menuPanel.setBackground(Color.pink);
         menuPanel.setVisible(true);
 
         JLabel mobileLabel = new JLabel("NOKLA");
         menuPanel.add(mobileLabel, new GridBagConstraints(
-                0, 0, 3, 1, 3,1,
+                0, 0, 2, 1, 2,1,
                 GridBagConstraints.CENTER, GridBagConstraints.CENTER,
                 new Insets(0,0,0,0),0,0));
         mobileLabel.setVisible(true);
 
-        messageLabel = new JLabel("!");
-        menuPanel.add(messageLabel, new GridBagConstraints(
+        typeMode = new JLabel("Abc");
+        menuPanel.add(typeMode, new GridBagConstraints(
+                2, 0, 1, 1, 1,1,
+                GridBagConstraints.CENTER, GridBagConstraints.CENTER,
+                new Insets(0,0,0,0),0,0));
+        typeMode.setVisible(true);
+
+        message = new JEditorPane();
+        message.setPreferredSize(new Dimension(270, 100));
+
+        menuPanel.add(message, new GridBagConstraints(
                 0, 1, 3, 2, 3,2,
                 GridBagConstraints.CENTER, GridBagConstraints.CENTER,
                 new Insets(0,0,0,0),0,0));
-        messageLabel.setVisible(true);
+        message.setVisible(true);
 
-        int dY = 1 + 2; // Значения gridheight у mobileLabel и messageLabel
+        int dY = 1 + 2; // Значения gridheight у mobileLabel и message
         int columns_of_buttons = 3;
-        int rows_of_buttons = buttonsName.length/columns_of_buttons; //4
+        int rows_of_buttons = (int) Math.ceil(buttonsName.length * 1.0/columns_of_buttons); //4
         int buttonsNameK = 0;
+        JButton[][] buttons = new JButton[rows_of_buttons][columns_of_buttons];
         for (int i = 0; i < rows_of_buttons; i++){
             for(int j = 0; j < columns_of_buttons; j++){
-                JButton button = new JButton(buttonsName[buttonsNameK]);
-                button.setPreferredSize(new Dimension(90,40));
-                button.addActionListener(new ButtonListener(buttonsName[buttonsNameK]));
-                menuPanel.add(button,
+                buttons[i][j] = new JButton(buttonsName[buttonsNameK]);
+                buttons[i][j].setPreferredSize(new Dimension(90,40));
+                if (buttonsParams.containsKey(buttons[i][j].getText())){
+                    ActionListener b1 = new ButtonListener(buttons[i][j].getText(), buttonsParams.get(buttons[i][j].getText()));
+                    buttons[i][j].addActionListener(b1);
+                }
+                menuPanel.add(buttons[i][j],
                         new GridBagConstraints(
                                 j, i + dY, 1, 1, 1,1,
                                 GridBagConstraints.CENTER, GridBagConstraints.CENTER,
                                 new Insets(0,0,0,0),0,0));
-                button.setVisible(true);
+                buttons[i][j].setVisible(true);
                 buttonsNameK++;
             }
         }
-        JButton clearButton = new JButton("Clear");
-        clearButton.setPreferredSize(new Dimension(90,40));
-        clearButton.addActionListener(new ButtonListener(clearButton.getText().toLowerCase()));
-        menuPanel.add(clearButton,
-                new GridBagConstraints(
-                        0, 8, 3, 1, 3,1,
-                        GridBagConstraints.CENTER, GridBagConstraints.CENTER,
-                        new Insets(0,0,0,0),0,0));
-        clearButton.setVisible(true);
-
         frame.add(menuPanel);
         frame.setLocationRelativeTo(null);
         frame.setFocusable(true);
         frame.pack();
         frame.setVisible(true);
     }
-
+    private String currentMode = valuesOfButtonStar[countMode];
     class ButtonListener implements ActionListener {
-        private String buttonText;
-        long startTime = System.currentTimeMillis();
+        private final String BUTTON_NAME;
+        private final String[] VALUES;
 
-        public ButtonListener(String buttonText){
-            this.buttonText = buttonText;
-            currentMsg = messageLabel.getText();
+        public ButtonListener(String buttonName, String[] values) {
+            BUTTON_NAME = buttonName.toLowerCase();
+            VALUES = values;
         }
 
-        private void counterCheck(int arrayLength){
-            if (arrayLength < count || !previousButtonText.equals(buttonText)){
-                count = 1;
-            }
-        }
-
-        private String timeCheck(long duration, String symbol){
-            if (duration >= 500){
-                currentMsg = messageLabel.getText();
-                count = 0;
-            } else {
-                if (duration >= 0){
-                    startTime = System.currentTimeMillis();
-                }
-            }
-            count++;
-            return currentMsg + symbol;
-        }
-
+        @Override
         public void actionPerformed(ActionEvent e) {
-            startTime = System.currentTimeMillis();
-            long duration = 0;
-            String symbol = "";
-            if (endTime != 0)
-                {duration = startTime - endTime;}
-            switch (buttonText) {
-                case "1": {
-                    String[] digit_1_values = {".",",","-","?","!","@","(",")","/","1"};
-                    counterCheck(digit_1_values.length);
-                    symbol = digit_1_values[count-1];
-                    break;
+            long startTime = System.currentTimeMillis();
+            String resultText = message.getText();
+            if (!previousButtonName.equals(BUTTON_NAME)) {
+                count = 0;
+                lastTime = 0;
+            }
+            if(VALUES != null) {
+                if (count > VALUES.length - 1) {
+                    count = 0;
                 }
-                case "2": {
-                    String[] digit_2_values = {"а","б","в","г","2"};
-                    counterCheck(digit_2_values.length);
-                    symbol = digit_2_values[count-1];
-                    break;
+                if (startTime - lastTime > 750) {
+                    count = 0;
+                    if(previousButtonName.equals(BUTTON_NAME) && currentMode.equals(valuesOfButtonStar[0])){
+                        currentMode = valuesOfButtonStar[1];
+                        countMode = 1;
+                        typeMode.setText(currentMode);
+                    }
+                    resultText += checkMode(VALUES[count], VALUES[VALUES.length-1]);
+                } else {
+                    if (startTime - lastTime >= 0) {
+                        resultText = cutOnOneSymbol(resultText) + checkMode(VALUES[count], VALUES[VALUES.length-1]);
+                    }
                 }
-                case "3": {
-                    String[] digit_3_values = {"д","е","ё","ж","з","3"};
-                    counterCheck(digit_3_values.length);
-                    symbol = digit_3_values[count-1];
-                    break;
-                }
-                case "4": {
-                    String[] digit_4_values = {"и","й","к","л","4"};
-                    counterCheck(digit_4_values.length);
-                    symbol = digit_4_values[count-1];
-                    break;
-                }
-                case "5": {
-                    String[] digit_5_values = {"м","н","о","п","5"};
-                    counterCheck(digit_5_values.length);
-                    symbol = digit_5_values[count-1];
-                    break;
-                }
-                case "6": {
-                    String[] digit_6_values = {"р","с","т","у","6"};
-                    counterCheck(digit_6_values.length);
-                    symbol = digit_6_values[count-1];
-                    break;
-                }
-                case "7": {
-                    String[] digit_7_values = {"ф","х","ц","ч","7"};
-                    counterCheck(digit_7_values.length);
-                    symbol = digit_7_values[count-1];
-                    break;
-                }
-                case "8": {
-                    String[] digit_8_values = {"ш","щ","Ъ","ы","8"};
-                    counterCheck(digit_8_values.length);
-                    symbol = digit_8_values[count-1];
-                    break;
-                }
-                case "9": {
-                    String[] digit_9_values = {"ь","э","ю","я","9"};
-                    counterCheck(digit_9_values.length);
-                    symbol = digit_9_values[count-1];
-                    break;
-                }
-                case "0": {
-                    String[] digit_0_values = {"+","=","<",">","$","%","0"};
-                    counterCheck(digit_0_values.length);
-                    symbol = digit_0_values[count-1];
-                    break;
-                }
-                case "*": {
-                    /*
-                    Первая заглавная остальные строчные включить UpperCase для первого нажатия
-                    Все заглавные включить UpperCase
-                    Все строчные отключить UpperCase
-                    Все цифры просто брать последний символ массива.
-                    */
-                    messageLabel.setText(messageLabel.getText()+"+");
-                    break;
-                }
-                case "#": {
-                    messageLabel.setText(messageLabel.getText()+" ");
-                    break;
-                }
-
-                case "clear": {
-                    messageLabel.setText("");
+                count++;
+            }  else {
+                switch (BUTTON_NAME){
+                    case "*": {
+                        if(countMode >= 3) {
+                            countMode = 0;
+                        } else {
+                            countMode++;
+                        }
+                        currentMode = valuesOfButtonStar[countMode];
+                        typeMode.setText(currentMode);
+                        break;
+                    }
+                    case "#": {
+                        resultText += " ";
+                        break;
+                    }
+                    case "clear": {
+                        resultText = "";
+                        countMode = 0;
+                        currentMode = valuesOfButtonStar[countMode];
+                        typeMode.setText(currentMode);
+                        break;
+                    }
+                    case "backspace": {
+                        resultText = cutOnOneSymbol(resultText);
+                        break;
+                    }
+                    case "enter": {
+                        resultText +="\n";
+                        countMode = 0;
+                        currentMode = valuesOfButtonStar[countMode];
+                        typeMode.setText(currentMode);
+                        break;
+                    }
                 }
             }
-            previousButtonText = this.buttonText;
-            String t = timeCheck(duration, symbol);
-            messageLabel.setText(t);
-            endTime = System.currentTimeMillis();
+            message.setText(resultText);
+            lastTime = System.currentTimeMillis();
+            previousButtonName = BUTTON_NAME;
+        }
+
+        private String cutOnOneSymbol(String text) {
+            return Optional.ofNullable(text)
+                    .filter(str -> str.length() != 0)
+                    .map(str -> str.substring(0, str.length() - 1))
+                    .orElse(text);
+        }
+
+        private String checkMode(String text, String number){
+            String modifyText = text;
+            switch (currentMode){
+                case "Abc":{
+                    if(!previousButtonName.equals("") && !previousButtonName.equals(BUTTON_NAME) && !"".equals(message.getText()) && !previousButtonName.equals("*")) {
+                        countMode = 1;
+                        currentMode = valuesOfButtonStar[countMode];
+                        typeMode.setText(currentMode);
+                        modifyText = text.toLowerCase();
+                    }
+                    else {
+                        modifyText = text.toUpperCase();
+                    }
+                    break;
+                }
+                case "abc":{
+                    modifyText = text.toLowerCase();
+                    break;
+                }
+                case "ABC":{
+                    modifyText = text.toUpperCase();
+                    break;
+                }
+                case "123":{
+                    modifyText = number;
+                    break;
+                }
+            }
+            return modifyText;
         }
     }
 }
